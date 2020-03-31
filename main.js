41ae44c95613021805e2241bf3d954c660a0a474
Cristiano Augusto <cristiano.asg@hotmail.com>
2020-03-31 19:50:10 +0100

    Arrow function para retorno de objeto

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-// Arrow function
+// Arrow function 
 
 const arr = [1, 3, 4, 5, 8, 9];
 
-const newArr = arr.map ((item) => {
-  return item * 2;
-});
+const newArr = arr.map (item => item * 2);
 
 console.log(newArr);
 
+// Arrow function para retorno de objeto
+var teste = () => ({ nome:'Cristiano'});
+
+console.log(teste());

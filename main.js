33187146544a67c546e6d93c6569bfb5d7d9614a
Cristiano Augusto <cristiano.asg@hotmail.com>
2020-03-31 23:17:39 +0100

    Definir valor padrao para parametros de funcoes

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-// Arrow function 
+// Definir valores padroes para parametros das funcoes
 
-const arr = [1, 3, 4, 5, 8, 9];
+const soma = ( a=3, b=6 ) => a + b;
 
-const newArr = arr.map (item => item * 2);
+console.log(soma(1));
+console.log(soma());
 
-console.log(newArr);
 
-// Arrow function para retorno de objeto
-var teste = () => ({ nome:'Cristiano'});
-
-console.log(teste());

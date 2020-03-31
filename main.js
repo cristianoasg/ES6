efef546fa5dc1be8789c667fb7da4bd4f1a885e1
Cristiano Augusto <cristiano.asg@hotmail.com>
2020-03-31 18:50:33 +0100

    Arrow function

--- main.js
+++ main.js
@@ -1,27 +1,10 @@
+// Arrow function
+
 const arr = [1, 3, 4, 5, 8, 9];
 
-const newArr = arr.map (function(item, index) {
-  return item + index;
+const newArr = arr.map ((item) => {
+  return item * 2;
 });
 
 console.log(newArr);
 
-//soma valores contidos no vetor
-
-const sum = arr.reduce(function(total, next) {
- return total + next;
-});
-
-console.log(sum);
-
-// Retorna valores pares contidos no array
-const filter = arr.filter(function(item) {
-  return item % 2 === 0;
-  });
-  console.log(filter);
-
-  // Verifica se existe o valor armazenado no array
-  const find = arr.find(function(item) {
-    return item === 2;
-  });
-  console.log(find);

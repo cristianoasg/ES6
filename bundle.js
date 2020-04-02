45e3c745c252642272e9be9398ac0c1a1c8366da
Cristiano Augusto <cristiano.asg@hotmail.com>
2020-04-02 23:51:22 +0100

    es6 - spread

--- bundle.js
+++ bundle.js
@@ -1,18 +1,17 @@
 "use strict";
 
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
 // rest
-var usuario = {
-  nome: 'Cristiano',
-  idade: 29,
-  funcao: 'estudante'
-};
+function soma(a, b) {
+  for (var _len = arguments.length, params = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    params[_key - 2] = arguments[_key];
+  }
 
-var nome = usuario.nome,
-    resto = _objectWithoutProperties(usuario, ["nome"]);
+  return params;
+}
 
-console.log(nome);
-console.log(resto);
+console.log(soma(1, 3, 4, 5, 6, 7)); // spread
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3);
--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 // rest
 
-const usuario = {
-
-  nome: 'Cristiano',
-  idade: 29,
-  funcao: 'estudante',
+function soma(a, b, ...params) {
+  return params;
+}
 
 
-};
+console.log(soma(1, 3, 4, 5, 6, 7));
 
-const { nome, ...resto } = usuario;
-console.log(nome);
-console.log(resto);
+
+// spread
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
